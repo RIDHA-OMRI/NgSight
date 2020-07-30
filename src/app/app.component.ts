63c555767e4ec521e5357b61e38d54804fb3ba5d
RIDHA OMRI <omriridha@hotmail.com>
2020-07-30 19:54:06 +0200

    navbar;sidebar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NgSightApp';
+  title = 'NgSight';
+  subtitle = 'Business Development Application with Angular';
 }
